3985a4a54ead5ebc431a39c869bc1d4a47c5d9f1
Indravathi732 <78473315+Indravathi732@users.noreply.github.com>
2024-02-14 19:22:28 +0530

    Add files via upload

---
+++ responsive-portfolio/js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Indravathi Ch</span><p>As a UI UX developer, I am passionate about designing beautiful, intuitive interfaces that help people be more productive. <br> I work with clients to create innovative and effective user interfaces that help them get the most out of their products. <br> As a UI UX designer, I am always striving to create interfaces that are easy to use and look beautiful. <br> I take into account the user’s needs and wants when designing my interfaces, and I always aim to create a positive user experience. <br> My approach to design is user-centric, and I always put the user first. I strive to create intuitive and easy-to-use interfaces that help users achieve their goals. <br> I am also passionate about making my designs responsive and adaptive to different devices and screen sizes. I'm always looking for new ways to enhance my skills.</p>" 
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 50);
